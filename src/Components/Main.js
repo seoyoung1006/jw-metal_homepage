c5287b366f76d5a86936bb9a3d2aec04249a1a98
toystory <74535088+seoyoung1006@users.noreply.github.com>
2023-03-09 17:09:48 +0900

    Make molding

--- src/Components/Main.js
+++ src/Components/Main.js
@@ -1,30 +1,46 @@
 import React, { Component } from "react";
+import present from "../image/sign.png";
 
 class Main extends Component {
   render(){
     return(
       <div class="main">
         <div class="about">
+          <h4 class="mtitle">
+            About Us
+          </h4>
           <div class="pic">
-            <img></img>
+            <img src={present} alt="company pic"/>
           </div>
           <div class="des">
             설명적기
           </div>
         </div>
         <div class="product">
+          <h4 class="mtitle">
+            Product
+          </h4>
           <div class="small">
-            <div class="pname"></div>
+            <h5 class="pname">
+              첫번째 상품
+            </h5>
             <div class="ppic">
-              <img></img>
+              <img src={present} alt="p1" />
             </div>
           </div>
           <div class="big">
-            <div class="pdes"></div>
-            <div class="pchart"></div>
+            <div class="pdes">
+              this help make desk~~
+            </div>
+            <div class="pchart">
+              chart니까 img?
+            </div>
           </div>
         </div>
         <div class="email">
+          <h4 class="mtitle">
+            Email to Us
+          </h4>
         </div>
       </div>
     );
